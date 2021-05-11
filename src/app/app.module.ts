bb9fa8567bdaf2ba068e3d0a106a2cca0545a665
chedi hammami <chedihammami9@gmail.com>
2021-05-11 21:40:43 +0100

    testing git 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProductsListComponent } from './components/products-list/products-list.
 import { StoresListComponent } from './components/stores-list/stores-list.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     ProductsListComponent,
     StoresListComponent,
     ContactUsComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule,
